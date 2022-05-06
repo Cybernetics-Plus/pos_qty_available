e9419a6c8faa369f1f8c795828f635b02ea9fa35
BOMEN <bomen21x@gmail.com>
2022-05-07 01:53:15 +0700

    Change icon

--- pos_qty_available/models/pos_config.py
+++ pos_qty_available/models/pos_config.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
-from functools import partial
 from odoo import models, fields
 
 class PosConfig(models.Model):
     _inherit = 'pos.config'
 
-    point_qty_available_id = fields.Boolean('Activate Module on this POS')
+    point_qty_available_id = fields.Boolean('Activate Module on this POS',default=True)
     point_qty_available_num = fields.Integer(string="Sequence", default=1)
-
