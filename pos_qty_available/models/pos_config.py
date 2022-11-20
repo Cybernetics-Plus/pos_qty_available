936a3489829bd71e648e9f026d2aecd1c0c4dbda
BOMEN <bomen21x@gmail.com>
2022-11-20 15:48:46 +0700

    pos_qty_available

--- pos_qty_available/models/pos_config.py
+++ pos_qty_available/models/pos_config.py
@@ -2,7 +2,7 @@
 from odoo import models, fields
 
 class PosConfig(models.Model):
-    _inherit = 'pos.config'
+    _inherit = "pos.config"
 
-    point_qty_available_id = fields.Boolean('Activate Module on this POS',default=True)
+    point_qty_available_id = fields.Boolean("Activate Module on this POS", default=False)
     point_qty_available_num = fields.Integer(string="Sequence", default=1)
